bfbe84d3c10afb6dc43bf5f979d331998f99dceb
null_ptr <spacecore4242@gmail.com>
2016-06-20 15:38:35 +0300

    add new task

---
+++ logoRecognition/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
